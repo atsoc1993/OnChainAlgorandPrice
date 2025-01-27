324b09ddd0af7ebc2d80e4ac75b344ea68d79865
atsoc1993 <144640214+atsoc1993@users.noreply.github.com>
2025-01-26 19:41:51 -0500

    Update generate_account.py

--- generate_account.py
+++ generate_account.py
@@ -6,8 +6,8 @@ load_dotenv()
 pk, address = generate_account()
 print(pk, address)
 
-set_key('.env', key_to_set='pk', value_to_set='pk')
-set_key('.env', key_to_set='address', value_to_set='address')
+set_key('.env', key_to_set='pk', value_to_set=pk)
+set_key('.env', key_to_set='address', value_to_set=address)
 
 # Use Algorand testnet dispenser to have MBR for deploying applications and fees for sending transactions
 # https://bank.testnet.algorand.network/
